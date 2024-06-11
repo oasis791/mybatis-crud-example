e205145b5abe917591160a0411bfec71f7da9431
oasis791 <khwi791@naver.com>
2024-06-11 16:14:00 +0900

    Initial Commit

---
+++ src/main/java/com/kimhyunwook/mybatiscommunity/MybatisCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.kimhyunwook.mybatiscommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kimhyunwook/mybatiscommunity/MybatisCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimhyunwook.mybatiscommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
