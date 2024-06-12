eb3bab3b9c22df5f5f8c70938cfd1fccf86c082a
oasis791 <khwi791@naver.com>
2024-06-12 14:59:42 +0900

    feat: 게시판 Repository 구현

---
+++ src/main/java/com/kimhyunwook/mybatiscommunity/repository/BoardRepository.java
@@ -0,0 +1,39 @@
+package com.kimhyunwook.mybatiscommunity.repository;
+
+import com.kimhyunwook.mybatiscommunity.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+
+    private final SqlSessionTemplate sql;
+
+    public void save(BoardDTO boardDTO) {
+        sql.insert("Board.save", boardDTO);
+    }
+
+    public List<BoardDTO> findAll() {
+        return sql.selectList("Board.findAll");
+    }
+
+    public void updateHits(Long id) {
+        sql.update("Board.updateHits", id);
+    }
+
+    public BoardDTO findById(Long id) {
+        return sql.selectOne("Board.findById", id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        sql.update("Board.update", boardDTO);
+    }
+
+    public void delete(Long id) {
+        sql.delete("Board.delete", id);
+    }
+}
