b49e184f0d74ce0a88d0622581e892862c1a8b4f
oasis791 <khwi791@naver.com>
2024-06-12 16:07:07 +0900

    feat: 다중 파일 저장을 위해 for문으로 순회 로직 추가

--- src/main/java/com/kimhyunwook/mybatiscommunity/service/BoardService.java
+++ src/main/java/com/kimhyunwook/mybatiscommunity/service/BoardService.java
@@ -19,7 +19,7 @@ public class BoardService {
 
     public void save(BoardDTO boardDTO) throws IOException {
         // 파일이 없다면
-        if (boardDTO.getBoardFile().isEmpty()) {
+        if (boardDTO.getBoardFile().get(0).isEmpty()) {
             boardDTO.setFileAttached(0);
             boardRepository.save(boardDTO);
         } else {
@@ -28,24 +28,25 @@ public class BoardService {
             // 게시글 저장 후, id값을 활용하기 위해 리턴 받는다.
             BoardDTO savedBoard = boardRepository.save(boardDTO);
             // 파일만 따로 가져온다.
-            MultipartFile boardFile = boardDTO.getBoardFile();
-            // 파일 이름 가져오기
-            String originalFilename = boardFile.getOriginalFilename();
-            // 저장용 이름 생성
-            String storedFileName = System.currentTimeMillis() + "-" + originalFilename;
+            for (MultipartFile boardFile : boardDTO.getBoardFile()) {
+                // 파일 이름 가져오기
+                String originalFilename = boardFile.getOriginalFilename();
+                // 저장용 이름 생성
+                String storedFileName = System.currentTimeMillis() + "-" + originalFilename;
 
-            // BoardFileDTO 세팅
-            BoardFileDTO boardFileDTO = new BoardFileDTO();
-            boardFileDTO.setOriginalFileName(originalFilename);
-            boardFileDTO.setStoredFileName(storedFileName);
-            boardFileDTO.setBoardId(savedBoard.getId());
+                // BoardFileDTO 세팅
+                BoardFileDTO boardFileDTO = new BoardFileDTO();
+                boardFileDTO.setOriginalFileName(originalFilename);
+                boardFileDTO.setStoredFileName(storedFileName);
+                boardFileDTO.setBoardId(savedBoard.getId());
 
-            // 파일 저장용 폴더에 파일 저장 처리
-            String filePath = "/Users/kimhyunwook/IdeaProjects/mybatis-community/upload_files/" + storedFileName;
-            boardFile.transferTo(new File(filePath));
+                // 파일 저장용 폴더에 파일 저장 처리
+                String filePath = "/Users/kimhyunwook/IdeaProjects/mybatis-community/upload_files/" + storedFileName;
+                boardFile.transferTo(new File(filePath));
 
-            // board_file_table에 저장 처리
-            boardRepository.saveFile(boardFileDTO);
+                // board_file_table에 저장 처리
+                boardRepository.saveFile(boardFileDTO);
+            }
         }
     }
 
@@ -69,7 +70,7 @@ public class BoardService {
         boardRepository.delete(id);
     }
 
-    public BoardFileDTO findFile(Long id) {
+    public List<BoardFileDTO> findFile(Long id) {
         return boardRepository.findFile(id);
     }
 }
