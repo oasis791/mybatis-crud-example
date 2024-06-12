c52c6005119d123b9fd3281c587b6d67dfa91978
oasis791 <khwi791@naver.com>
2024-06-12 14:59:02 +0900

    feat: 게시판 DTO CRUD 서비스 구현

---
+++ src/main/java/com/kimhyunwook/mybatiscommunity/service/BoardService.java
@@ -0,0 +1,39 @@
+package com.kimhyunwook.mybatiscommunity.service;
+
+import com.kimhyunwook.mybatiscommunity.dto.BoardDTO;
+import com.kimhyunwook.mybatiscommunity.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public void save(BoardDTO boardDTO) {
+        boardRepository.save(boardDTO);
+    }
+
+    public List<BoardDTO> findAll() {
+        return boardRepository.findAll();
+    }
+
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDTO findById(Long id) {
+        return boardRepository.findById(id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        boardRepository.update(boardDTO);
+    }
+
+    public void delete(Long id) {
+        boardRepository.delete(id);
+    }
+}
