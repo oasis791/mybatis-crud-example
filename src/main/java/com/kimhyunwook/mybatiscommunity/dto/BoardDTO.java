9ad60566d9e8ef3f114d5f8b5b4f244a273a80f9
oasis791 <khwi791@naver.com>
2024-06-12 14:58:39 +0900

    feat: 게시판 DTO 생성

---
+++ src/main/java/com/kimhyunwook/mybatiscommunity/dto/BoardDTO.java
@@ -0,0 +1,21 @@
+package com.kimhyunwook.mybatiscommunity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class BoardDTO {
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContents;
+    private int boardHits;
+    private String createdAt;
+}
