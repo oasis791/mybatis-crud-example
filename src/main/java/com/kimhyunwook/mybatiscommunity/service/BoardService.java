e29a963038088170732041724a5201fbdfc62113
oasis791 <khwi791@naver.com>
2024-06-12 15:24:59 +0900

    feat: 게시글 저장 기능에 File의 유무에 따라 로직 분리

--- src/main/java/com/kimhyunwook/mybatiscommunity/service/BoardService.java
+++ src/main/java/com/kimhyunwook/mybatiscommunity/service/BoardService.java
@@ -1,10 +1,14 @@
 package com.kimhyunwook.mybatiscommunity.service;
 
 import com.kimhyunwook.mybatiscommunity.dto.BoardDTO;
+import com.kimhyunwook.mybatiscommunity.dto.BoardFileDTO;
 import com.kimhyunwook.mybatiscommunity.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -13,8 +17,36 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
-    public void save(BoardDTO boardDTO) {
-        boardRepository.save(boardDTO);
+    public void save(BoardDTO boardDTO) throws IOException {
+        // 파일이 없다면
+        if (boardDTO.getBoardFile().isEmpty()) {
+            boardDTO.setFileAttached(0);
+            boardRepository.save(boardDTO);
+        } else {
+            // 파일이 있다면
+            boardDTO.setFileAttached(1);
+            // 게시글 저장 후, id값을 활용하기 위해 리턴 받는다.
+            BoardDTO savedBoard = boardRepository.save(boardDTO);
+            // 파일만 따로 가져온다.
+            MultipartFile boardFile = boardDTO.getBoardFile();
+            // 파일 이름 가져오기
+            String originalFilename = boardFile.getOriginalFilename();
+            // 저장용 이름 생성
+            String storedFileName = System.currentTimeMillis() + "-" + originalFilename;
+
+            // BoardFileDTO 세팅
+            BoardFileDTO boardFileDTO = new BoardFileDTO();
+            boardFileDTO.setOriginalFileName(originalFilename);
+            boardFileDTO.setStoredFileName(storedFileName);
+            boardFileDTO.setBoardId(savedBoard.getId());
+
+            // 파일 저장용 폴더에 파일 저장 처리
+            String filePath = "/Users/kimhyunwook/IdeaProjects/mybatis-community/upload_files/" + storedFileName;
+            boardFile.transferTo(new File(filePath));
+
+            // board_file_table에 저장 처리
+            boardRepository.saveFile(boardFileDTO);
+        }
     }
 
     public List<BoardDTO> findAll() {
@@ -35,5 +67,9 @@ public class BoardService {
 
     public void delete(Long id) {
         boardRepository.delete(id);
+    }
+
+    public BoardFileDTO findFile(Long id) {
+        return boardRepository.findFile(id);
     }
 }
